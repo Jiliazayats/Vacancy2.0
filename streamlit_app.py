3d443356401faa6f9e6b8a31c27f6f70f10a7344
Jiliazayats <jiliazayats@gmail.com>
2024-10-21 20:41:19 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,6 @@ client = openai.Client(
     api_key=os.getenv("OPENAI_API_KEY")
 )
 
-
 SYSTEM_PROMPT = """
 Проскорь кандидата, насколько он подходит для данной вакансии.
 
@@ -17,7 +16,6 @@ SYSTEM_PROMPT = """
 Отдельно оцени качество заполнения резюме (понятно ли, с какими задачами сталкивался кандидат и каким образом их решал?). Эта оценка должна учитываться при выставлении финальной оценки - нам важно нанимать таких кандидатов, которые могут рассказать про свою работу
 Потом представь результат в виде оценки от 1 до 10.
 """.strip()
-
 
 def request_gpt(system_prompt, user_prompt):
     response = client.chat.completions.create(
@@ -31,25 +29,25 @@ def request_gpt(system_prompt, user_prompt):
     )
     return response.choices[0].message.content
 
+st.title("Приложение для оценки резюме")
 
-st.title("CV Scoring App")
+job_description_url = st.text_area("Введите URL описания вакансии")
 
-job_description_url = st.text_area("Enter the job description url")
+cv_url = st.text_area("Введите URL резюме")
 
-cv_url = st.text_area("Enter the CV url")
-
-if st.button("Score CV"):
-    with st.spinner("Scoring CV..."):
+if st.button("Оценить резюме"):
+    with st.spinner("Оценка резюме..."):
 
         job_description = get_job_description(job_description_url)
         cv = get_candidate_info(cv_url)
 
-        st.write("Job description:")
+        st.write("### Описание вакансии:")
         st.write(job_description)
-        st.write("CV:")
+        st.write("### Резюме кандидата:")
         st.write(cv)
 
         user_prompt = f"# ВАКАНСИЯ\n{job_description}\n\n# РЕЗЮМЕ\n{cv}"
         response = request_gpt(SYSTEM_PROMPT, user_prompt)
 
+    st.write("### Результат оценки:")
     st.write(response)
